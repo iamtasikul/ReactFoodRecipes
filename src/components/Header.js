af930f4778b824e8ca7a91aa8faf49db36fcbb74
Tasikul Islam <iamtasikul@gmail.com>
2020-10-20 17:56:58 +0530

    SearchBar added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Header=()=>{
     return(
-        <div className="jumbotron">
-            <h1 className="display-1">Food Recipes</h1>
+        <div className="jumbotron py-5">
+            <h1 className="display-1"><span class="material-icons brand-icon">fastfood</span>Food Recipes</h1>
+            <div class="input-group w-50 mx-auto">
+                <input type="text" placeholder="Find Your recipes..." class="form-control" /> 
+                <div class="input-group-append">
+                <button className="btn btn-dark">Search Recipes</button>
+                </div>
+            </div>
         </div>
     );
 }
