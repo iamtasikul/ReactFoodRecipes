f72523622b44f184be49f74f30494063c8906707
Tasikul Islam <iamtasikul@gmail.com>
2020-10-21 16:05:09 +0530

    Updates

---
+++ precache-manifest.78c5882aa2a781383dc0c4c2bf0b3e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f37f337e1faf29add2363b15deb1dd",
+    "url": "/ReactFoodRecipes/index.html"
+  },
+  {
+    "revision": "34f77cf556f806a9a433",
+    "url": "/ReactFoodRecipes/static/css/main.d687ca62.chunk.css"
+  },
+  {
+    "revision": "6a295ae9284227dbf9d2",
+    "url": "/ReactFoodRecipes/static/js/2.d41c2e39.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ReactFoodRecipes/static/js/2.d41c2e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34f77cf556f806a9a433",
+    "url": "/ReactFoodRecipes/static/js/main.5c47c2dd.chunk.js"
+  },
+  {
+    "revision": "d554b34c374a82ea7752",
+    "url": "/ReactFoodRecipes/static/js/runtime-main.310261ab.js"
+  }
+]);
